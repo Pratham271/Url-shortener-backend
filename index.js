a72d215ffbf7061b92b540a663a9674a63b0c71f
Pratham Chauhan <83640434+Pratham271@users.noreply.github.com>
2024-03-11 15:53:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors)
 const urlShortenRouter = require('./routes/urlShorten')
+
 const app = express();
 // app.use(bodyParser.json());
+app.use(cors());
 app.use(express.json());
 
 app.use("/",urlShortenRouter)
